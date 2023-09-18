2b91c360990cf8d7fe6698ed93438da84104ed1e
Nathaniel Liu <71233101+NatL7499@users.noreply.github.com>
2023-09-17 21:36:25 -0500

    Added dataset and formatting in index.js

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,44 @@
 import express from "express"
 import cors from "cors"
+import fs from "fs";
+import readline from "readline";
 
 const app = express()
 
-app.use(cors({
-    origin: "http://localhost:5500"
-}))
+app.use(cors());
 
 app.get("/", (req, res) => {
-    res.send({
-        x: [1, 2, 3],
-        y: [4, 5, 6]
-    }).status(200)
-})
+    const data = [];
+    const filePath = "player_stats.csv";
+
+    const readL = readline.createInterface({
+        input: fs.createReadStream(filePath),
+        output: process.stdout,
+        terminal: false
+    });
+
+    let lineCt = 0;
+
+    readL.on("line", (line) => {
+        if(lineCt != 0 || lineCt>200){
+            const values = line.split(",");
+        
+            const kd = parseFloat(values[6]);
+            const rating = parseFloat(values[7]);
+            data.push({kd, rating});
+        }
+        lineCt++;
+    });
+
+    readL.on("close", () => {
+        res.json(data);
+    });
+
+    readL.on("error", (error) => {
+        console.error("Error reading CSV file:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    });
+});
 
 
 const port = process.env.PORT || 3000
--- frontend/graph.mjs
+++ frontend/graph.mjs
@@ -1,8 +1,27 @@
 const graphDiv = document.getElementById("graph");
 
 fetch(
-    "https://oa-2023-24-backend.onrender.com" //use "http://localhost:3000" if running sample express backend locally, or replace with your own backend endpoint url
+    "https://dssd23-24.onrender.com" //use "http://localhost:3000" if running sample express backend locally, or replace with your own backend endpoint url
 ).then(async res => {
-    Plotly.newPlot( graphDiv, [ await res.json() ]); 
-})
+    const data = await res.json();
 
+    const plot = {
+        x: data.map(item => item.kd),
+        y: data.map(item => item.rating),
+        mode: 'markers',
+        type: 'scatter',
+        marker: {size: 5}
+    };
+
+    const legend = {
+        title: 'CSGO KD vs HLTV Rating',
+        xaxis:{
+            title:'Kill Death Ratio',
+        },
+        yaxis:{
+            title: 'HLTV Rating',
+        }
+    };
+
+    Plotly.newPlot(graphDiv, [plot], legend);
+})
