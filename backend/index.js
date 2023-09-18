db2cb2900d1926fb65111de075c14a8b78de8bb2
Nathaniel Liu <71233101+NatL7499@users.noreply.github.com>
2023-09-18 03:07:05 -0500

    changed dataset structure

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,6 @@ app.get("/", (req, res) => {
     readL.on("line", (line) => {
         if(lineCt != 0 && lineCt < 50){
             const values = line.split(",");
-            console.log(line);
         
             const kd = parseFloat(values[6]);
             const rating = parseFloat(values[7]);
